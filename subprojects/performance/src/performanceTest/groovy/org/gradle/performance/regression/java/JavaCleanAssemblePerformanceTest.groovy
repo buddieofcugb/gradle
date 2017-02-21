458341e896db07c811d82b1b47fe1e2e57c656c6
Jendrik Johannes <jendrik@gradle.com>
2017-02-21 15:58:17 +0100

    Simplify Java scenario: clean assemble
    Removes tests that tested similar functionality with other more
    unrealistic projects or tested only parts of the functionality.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaCleanAssemblePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaCleanAssemblePerformanceTest.groovy
@@ -21,15 +21,11 @@ import spock.lang.Unroll
 
 class JavaCleanAssemblePerformanceTest extends AbstractCrossVersionPerformanceTest {
 
-    @Unroll("clean assemble Java project - #testProject")
-    def "clean assemble Java project"() {
+    @Unroll
+    def "#testProject"() {
         given:
-        runner.testId = "clean assemble $testProject project" + (parallel ? " (parallel)" : "")
-        runner.previousTestIds = parallel ? [] : ["full assemble Java build $testProject (daemon)"]
-        runner.testProject = testProject
         runner.tasksToRun = ["clean", "assemble"]
-        runner.gradleOpts = ["-Xms${maxMemory}", "-Xmx${maxMemory}"]
-        runner.args = parallel ? ['-Dorg.gradle.parallel=true', '-Dorg.gradle.parallel.intra=true'] : []
+        runner.gradleOpts = ["-Xms${memory}", "-Xmx${memory}"]
         runner.warmUpRuns = warmUpRuns
         runner.runs = runs
 
@@ -40,12 +36,8 @@ class JavaCleanAssemblePerformanceTest extends AbstractCrossVersionPerformanceTe
         result.assertCurrentVersionHasNotRegressed()
 
         where:
-        testProject            | maxMemory | parallel | warmUpRuns | runs
-        "bigOldJavaMoreSource" | '608m'    | false    | 2          | 6
-        "bigOldJava"           | '576m'    | false    | 2          | 6
-        "mediumOldJava"        | '128m'    | false    | null       | null
-        "smallOldJava"         | '128m'    | false    | null       | null
-        "largeEnterpriseBuild" | '2g'      | false    | 2          | 6
-        "largeEnterpriseBuild" | '2g'      | true     | 2          | 6
+        testProject                  | memory | warmUpRuns | runs
+        "largeMonolithicProjectJava" | '608m' | 2          | 6
+        "largeMultiProjectJava"      | '608m' | 2          | 6
     }
 }
